3503a8c7598c8f5fb41b5e0f909a587ccac3e9ed
patarapolw <patarapolw@gmail.com>
2018-02-02 23:33:25 +0700

    Add community module

---
+++ community/community.py
@@ -0,0 +1,45 @@
+import os
+import requests
+from website import login
+from time import time
+
+class discourse:
+    def __init__(self, username='', password=''):
+        with login.web(username, password) as w:
+            w.driver.get('https://community.wanikani.com')
+            cookie = w.driver.get_cookies()
+            self.session = requests.Session()
+            c = [self.session.cookies.set(c['name'], c['value']) for c in cookie]
+
+    def GET(self, end_point, params={}):
+        url = 'https://community.wanikani.com/{}.json'.format(end_point)
+        response = self.session.get(url, params=params)
+        return response.text
+
+
+if __name__ == '__main__':
+    os.chdir('..')
+
+    start = time()
+    board = discourse()
+    print('Time elasped: {} seconds'.format(time()-start)) # 56.1 seconds
+
+    id = 10
+    flag = ("all", "yearly", "quarterly", "monthly", "weekly", "daily")
+    username = 'polv'
+    end_point = (
+        'categories', # Get a list of categories
+        'c/{}'.format(id), # Get a list of topics in the specified category
+        't/{}'.format(id), # Get a single topic
+        'latest', # Get the latest topics
+        'top', # Get the top topics
+        'top/{}'.format(flag[0]), # Get the top topics filtered by specified flag
+        'post/{}'.format(id), # Get a single post
+        'user/{}'.format(username), # Get a single user by username
+        'directory_items', # get a public list of users. Requires params: period, order
+        'admin/users/list/{}'.format(flag), # returns a list of users
+        'user_actions',
+
+    )
+
+    print(board.GET('categories'))
---
+++ example/multiple_readings.py
@@ -0,0 +1,38 @@
+import json
+import jaconv
+import os
+os.chdir('..')
+from api import api
+
+api_v2 = api.v2()
+result = api_v2.GET('subjects', params={'types':'vocabulary'})
+
+# print(json.dumps(result, indent=4))
+while True:
+    for data in result['data']:
+        reading_array = set()
+        for reading in data["data"]["readings"]:
+            # if reading['primary']:
+                reading_array.add(jaconv.kata2hira(reading['reading']))
+
+        if len(reading_array) == 1:
+            continue
+
+        meaning_array = []
+        for meaning in data["data"]["meanings"]:
+            if meaning['primary']:
+                meaning_array += [meaning['meaning']]
+
+        to_print = (
+            data["data"]["characters"] if "characters" in data["data"] else data["data"]["character"],
+            ', '.join(reading_array),
+            ', '.join(meaning_array)
+        )
+        print('\t'.join(to_print))
+
+    next_url = result['pages']['next_url']
+
+    if next_url is None:
+        break
+
+    result = api_v2.GETurl(next_url)
