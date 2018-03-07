dcae69ac3fbf2adfa143d78590b6e9b7dcd2bdf5
patarapolw <patarapolw@gmail.com>
2018-03-07 07:08:17 +0700

    Community API without selenium works!

--- WaniKaniTools/community/community.py
+++ WaniKaniTools/community/community.py
@@ -21,23 +21,24 @@ if __name__ == '__main__':
 
     start = time()
     board = Discourse()
-    print('Time elasped: {} seconds'.format(time()-start)) # 56.1 seconds
+    print('Time elasped: {} seconds'.format(time()-start))
 
     id = 10
     flag = ("all", "yearly", "quarterly", "monthly", "weekly", "daily")
     username = 'polv'
     end_point = (
-        'categories', # Get a list of categories
-        'c/{}'.format(id), # Get a list of topics in the specified category
-        't/{}'.format(id), # Get a single topic
-        'latest', # Get the latest topics
-        'top', # Get the top topics
-        'top/{}'.format(flag[0]), # Get the top topics filtered by specified flag
-        'post/{}'.format(id), # Get a single post
-        'users/{}'.format(username), # Get a single user by username
-        'directory_items', # get a public list of users. Requires params: period, order
-        'admin/users/list/{}'.format(flag), # returns a list of users
-        'user_actions'
+        'categories',  # Get a list of categories
+        'c/{}'.format(id),  # Get a list of topics in the specified category
+        't/{}'.format(id),  # Get a single topic
+        'latest',  # Get the latest topics
+        'top',  # Get the top topics
+        'top/{}'.format(flag[0]),  # Get the top topics filtered by specified flag
+        'post/{}'.format(id),  # Get a single post
+        'users/{}'.format(username),  # Get a single user by username
+        'directory_items',  # get a public list of users. Requires params: period, order
+        'admin/users/list/{}'.format(flag),  # returns a list of users
+        'user_actions',
+        # and so on
     )
 
     print(board.GET('users/polv'))
